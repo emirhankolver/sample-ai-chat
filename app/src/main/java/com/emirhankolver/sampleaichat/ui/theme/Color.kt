cde89faf0d39cb239dae925b9692763501431726
Emirhan <emirhanklvr@gmail.com>
2025-01-23 17:56:59 +0300

    #1 Build an Engaging Splash Screen Color Schemes have been changed. SDK versions are upgraded.

--- app/src/main/java/com/emirhankolver/sampleaichat/ui/theme/Color.kt
+++ app/src/main/java/com/emirhankolver/sampleaichat/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.emirhankolver.sampleaichat.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Aqua80 = Color(0xff00D2D3)
+val Blue80 = Color(0xff54A0FF)
+val Purple80 = Color(0xff54A0FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Aqua40 = Color(0xff01A3A4)
+val Blue40 = Color(0xff2E86DE)
+val Purple40 = Color(0xff341F97)
